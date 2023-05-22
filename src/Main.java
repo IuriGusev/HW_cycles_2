7b18a9870905787ca2be9b935d75862981c5cd72
iurii_gusev <gusevyuv@gmail.com>
2023-05-22 11:22:14 -0400

    HW_cycles_2 was made

--- src/Main.java
+++ src/Main.java
@@ -12,12 +12,12 @@ public class Main {
 
     public static void task1() {
         System.out.println("task1");
-        int salary = 0;
-        int month = 0;
-        while (salary < 2459001) {
+        int salary = 15_000;
+        int month = 1;
+        while (salary <= 2_459_000) {
             System.out.println("Месяц " + month + "," + "сумма накоплений равна " + salary + " рублей");
-            salary = salary + 15000;
-            month = month + 1;
+            salary = salary + 15_000;
+            month += 1;
         }
         System.out.println();
     }
@@ -25,9 +25,9 @@ public class Main {
     public static void task2() {
         System.out.println("task2");
         int start = 1;
-        while (start < 11) {
+        while (start <= 10) {
             System.out.print(start + " ");
-            start = start + 1;
+            start += 1;
         }
         System.out.println();
 
@@ -39,12 +39,12 @@ public class Main {
 
     public static void task3() {
         System.out.println("task3");
-        int startPopulation = 12000000;
+        int startPopulation = 12_000_000;
         int birthRateEvery1000 = 17 - 8;
-        int year = 0;
-        while (year < 11) {
+        int year = 1;
+        while (year <= 10) {
             System.out.println("Ear " + year + "," + " amount of population is " + startPopulation);
-            year = year + 1;
+            year += 1;
             startPopulation = startPopulation + (startPopulation / 1000 * birthRateEvery1000);
         }
         System.out.println();
@@ -52,26 +52,22 @@ public class Main {
 
     public static void task4() {
         System.out.println("task4");
-        int salary = 15000;
-        int month = 0;
-        int everyMonthBankPercentSaving = (salary * 7 * 30 / 365) / 100;
-        while (salary < 12_000_001) {
+        double salary = 15_000;
+        int month = 1;
+        while (salary <= 12_000_000) {
             System.out.println("Месяц " + month + "," + "сумма накоплений равна " + salary + " рублей");
-            salary = salary + 15000 + everyMonthBankPercentSaving;
-            month = month + 1;
+            salary = salary + salary*0.07;
+            month += 1;
         }
         System.out.println();
     }
 
     public static void task5() {
         System.out.println("task5");
-        int salary = 15_000;
-        int month = 0;
-        int everyMonthAmountSaving = salary + 15_000;
-        int bankPercentSaving = (salary * 7 * 30 / 365) / 100;
-        for (; salary < 12_000_000; month++) {
-            salary = salary + everyMonthAmountSaving;
-            salary = bankPercentSaving + salary;
+        double salary = 15_000.00;
+        int month = 1;
+        for (; salary <= 12_000_000; month++) {
+            salary = salary + salary*0.07;
             if (month % 6 == 0) {
                 System.out.println("Месяц " + month + " Итого " + salary);
             }
@@ -80,13 +76,10 @@ public class Main {
 
     public static void task6() {
         System.out.println("task6");
-        int salary = 15_000;
-        int month = 0;
-        int everyMonthAmountSaving = salary + 15_000;
-        int bankPercentSaving = (salary * 7 * 30 / 365) / 100;
-        for (; month < 109; month++) {
-            salary = salary + everyMonthAmountSaving;
-            salary = bankPercentSaving + salary;
+        double salary = 15_000;
+        int month = 1;
+        for (; month <= 9*12; month++) {
+            salary = salary + salary*0.07;
             if (month % 6 == 0) {
                 System.out.println("Месяц " + month + " Итого " + salary);
             }
@@ -95,14 +88,13 @@ public class Main {
 
     public static void task7() {
         System.out.println("task7");
-        int day = 3;
-        for (; day < 32; day = day + 7)
+        for (int day = 3; day <= 31; day = day + 7) {
             System.out.println("Сегодня пятница, " + day + "-е число.Необходимо подготовить отчет");
-    }
+    }}
 
     public static void task8() {
         System.out.println("Task8");
-        for (int i = 0; i < (2023+100); i = i + 79)
+        for (int i = 0; i < (2023+100); i += 79)
             if (i>(2023-200))
             System.out.println(i);
     }
